389f18d99be0ba55bbf1ee9c622386d888178171
mglowinski <mck00@o2.pl>
2020-03-08 12:15:56 +0100

    add role model

---
+++ src/main/java/com/mglowinski/sso/model/Role.java
@@ -0,0 +1,17 @@
+package com.mglowinski.sso.model;
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Data
+@Entity
+@Table(name = "roles")
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String role;
+}
