8784d487bc2d0d9535fe59f936ab217f457fd550
mglowinski <mck00@o2.pl>
2020-03-08 12:15:39 +0100

    add user model

---
+++ src/main/java/com/mglowinski/sso/model/User.java
@@ -0,0 +1,27 @@
+package com.mglowinski.sso.model;
+
+import lombok.Data;
+
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
+
+@Data
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String userName;
+    private String password;
+    private boolean isActive;
+
+    @ManyToMany(cascade = {CascadeType.PERSIST, CascadeType.MERGE})
+    @JoinTable(name = "users_roles",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id"))
+    private Set<Role> roles = new HashSet<>();
+}
